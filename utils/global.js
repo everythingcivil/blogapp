e75980e7d08017788a5b1ad04aa1d5f11fdad2f6
Pankaj Sarkar <driveworking@gmail.com>
2021-05-11 01:32:32 +0530

    commit

--- utils/global.js
+++ utils/global.js
@@ -7,6 +7,6 @@ export default {
   githubURL: 'https://github.com/',
   siteTitle: 'Everything Civil Blog - A Civil Engineering Knowledge Base',
   siteDesc: 'A description for your site here, this will show on the home page.',
-  mainImage: 'https://media-exp1.licdn.com/dms/image/C4E12AQGPt9AfPaKNHA/article-inline_image-shrink_1000_1488/0/1520600904563?e=1626307200&v=beta&t=fgsNw4wLK_c5fgjcMaBdLe6IsRcpmQgTiS10sKmcakw',
+  mainImage: '/uploads/main-image.jpg',
   siteType: 'website',
 }
